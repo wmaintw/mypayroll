ded9b448c5b4bd6598b89244a55d44910bd0d667
ma wei <wma@thoughtworks.com>
2013-02-15 14:45:18 +0800

    init project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypayroll::Application.config.secret_token = '720effcb850ee0241ee7457091ba3f2fe621210df0b3a126ddb2439587a29730c32adcadd3cf73aeaffe7e5a6b82c2b3c6d4222ba3d982364bf4858329db506b'
